c8bcd66b49da1877839c972a16883266edfaf8ba
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-10-15 15:03:53 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperVirusLandSimulationExtream_KatelynNicholson
+{
+    //virus =  = X
+    //virus (x, y) build index for each virus that spawns
+    //grass = green = -
+    //water = blue = ~ 
+    //mountains = Brown = ^
+    //3 starting viruses on grass
+
+    internal class Program
+    {
+
+        static string virusAvatar = "X";
+        static string grassAvatar = "-";
+        static string waterAvatar = "~";
+        static string mountainAvatar = "^";
+        static bool[,] map = new bool[20,20];
+
+        static void Main()
+        {
+
+            PrintMap();
+        }
+
+        static void PrintMap()
+        {
+            for (int y = 0; y < map.GetLength(0); y++)
+            {
+
+                for (int x = 1; x < map.GetLength(1); x++)
+                {
+                    bool tile = map[y,x];
+                    
+                    switch (tile)
+                    {
+                        case '-':
+                            Console.BackgroundColor = ConsoleColor.Green;
+                            break;
+                        case '~':
+                            Console.BackgroundColor = ConsoleColor.Blue;
+                            break;
+                        case '^':
+                            Console.BackgroundColor = ConsoleColor.Gray;
+                            break;
+                    }
+                    Console.Write(tile);
+                }
+            }
+        }
+
+    }
+}
